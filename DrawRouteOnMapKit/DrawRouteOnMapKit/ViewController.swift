fd49a319e516ad67a74454106bb175f2ae6ba41b
jabeedkhan <57361094+jabeedkhan@users.noreply.github.com>
2019-11-17 16:36:55 +0530

    Add files via upload

---
+++ DrawRouteOnMapKit/DrawRouteOnMapKit/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  DrawRouteOnMapKit
+//
+//  Created by jabeed on 10/10/19.
+//  Copyright © 2019 jabeed. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class customPin: NSObject, MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(pinTitle:String, pinSubTitle:String, location:CLLocationCoordinate2D) {
+        self.title = pinTitle
+        self.subtitle = pinSubTitle
+        self.coordinate = location
+    }
+}
+
+class ViewController: UIViewController,MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude:13.063141 , longitude: 77.620674)
+        let destinationLocation = CLLocationCoordinate2D(latitude:12.972369 , longitude: 77.637911)
+        
+        let sourcePin = customPin(pinTitle: "Jakkur", pinSubTitle: "", location: sourceLocation)
+        let destinationPin = customPin(pinTitle: "Indiranagar", pinSubTitle: "", location: destinationLocation)
+        self.mapView.addAnnotation(sourcePin)
+        self.mapView.addAnnotation(destinationPin)
+        
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let directionResonse = response else {
+                if let error = error {
+                    print("we have error getting directions==\(error.localizedDescription)")
+                }
+                return
+            }
+            
+            let route = directionResonse.routes[0]
+            self.mapView.addOverlay(route.polyline, level: .aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+        
+        self.mapView.delegate = self
+        
+        
+    }
+    
+    //MARK:- MapKit delegates
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = #colorLiteral(red: 1, green: 0.299052978, blue: 0.1324341589, alpha: 1)
+        renderer.lineWidth = 10.0
+        return renderer
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+    
+
+
+}
